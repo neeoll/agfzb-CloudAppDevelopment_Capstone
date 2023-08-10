9d93078e2c721f5b71c9b6dbe15f0a164601b1c1
neeoll <arias.noel24@gmail.com>
2023-08-10 13:29:19 -0400

    User Management

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,16 +8,17 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-    path('', views.index, name='index' ),
+    
     # path for about view
     path('about/', views.about, name='about'),
     # path for contact us view
     path('contact/', views.contact, name='contact'),
     # path for registration
-
+    
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
