5c8fbbf1c125f9c1f2a8eb13fd4fe2ac82f371be
neeoll <arias.noel24@gmail.com>
2023-08-18 15:03:54 -0400

    Create Django Proxy Services of Cloud Functions

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,6 +9,7 @@ from django.contrib import messages
 from datetime import datetime
 import logging
 import json
+from .restapis import get_dealers_from_cf
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -93,10 +94,11 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
-
+    if (request.method == "GET"):
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/846eb439-3586-4d7d-82bd-3f206faf52b5/dealerships/get-dealerships.json"
+        dealerships = get_dealers_from_cf(url)
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        return HttpResponse(dealer_names)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
